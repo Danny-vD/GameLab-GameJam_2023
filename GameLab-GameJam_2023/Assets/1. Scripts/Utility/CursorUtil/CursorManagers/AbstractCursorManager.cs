5968cee32fa70b8d4e1278447c0057d8ae5297e9
Danny-vD <dvduist@hotmail.nl>
2023-05-04 12:02:43 +0200

    Add serializableDictionary package and some basic Utility&Editor scripts

---
+++ GameLab-GameJam_2023/Assets/1. Scripts/Utility/CursorUtil/CursorManagers/AbstractCursorManager.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using VDFramework;
+using Utility.SerializableDictionary;
+using MouseButton = Utility.CursorUtil.MouseUtil.MouseButton;
+
+namespace Utility.CursorUtil.CursorManagers
+{
+	[DisallowMultipleComponent]
+	public abstract class AbstractCursorManager<T> : BetterMonoBehaviour where T : struct
+	{
+		[SerializeField]
+		protected T idleDatum;
+		
+		[SerializeField, Tooltip("Order matters, higher placed will be shown over lower")]
+		private SerializableDictionary<MouseButton, T> MouseClickData;
+		
+		public bool IsIdle { get; private set; }
+
+		public MouseButton CurrentState { get; private set; }
+		
+		private void Update()
+		{
+			foreach ((MouseButton button, T datum) in MouseClickData)
+			{
+				if (MouseUtil.IsButtonPressed(button))
+				{
+					if (!IsIdle && CurrentState == button) // If we're currently idle, always go through
+					{
+						// Prevent constantly setting the current state to itself
+						return;
+					}
+
+					SetState(button, datum);
+					return;
+				}
+			}
+
+			if (!IsIdle)
+			{
+				SetState(default, idleDatum, true);
+			}
+		}
+
+		private void SetState(MouseButton button, T value, bool idle = false)
+		{
+			CurrentState = button;
+			IsIdle       = idle;
+			
+			OnStateChanged(CurrentState, value, IsIdle);
+		}
+
+		protected abstract void OnStateChanged(MouseButton newState, T datum, bool idle = false);
+	}
+}
