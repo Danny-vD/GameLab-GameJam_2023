d70eed1db37fa11eff52e8ea170625375846f417
Danny-vD <dvduist@hotmail.nl>
2023-05-04 12:04:07 +0200

    Add JSON Localisation Package

---
+++ GameLab-GameJam_2023/Assets/1. Scripts/Localisation/UI/LocalisedText.cs
@@ -0,0 +1,62 @@
+﻿using Events.Localisation;
+using IO.Localisation;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using VDFramework;
+using VDFramework.EventSystem;
+
+namespace Localisation.UI
+{
+	public class LocalisedText : BetterMonoBehaviour
+	{
+		[SerializeField, Tooltip("Will use the current text as the textType variable")]
+		private bool UseTextAsEntryID = false;
+
+		[SerializeField]
+		private string textType = "PLACEHOLDER";
+
+		private Text text;
+		private TMP_Text textTMP;
+
+		private void Awake()
+		{
+			text    = GetComponent<Text>();
+			textTMP = GetComponent<TMP_Text>();
+		}
+
+		private void Start()
+		{
+			if (UseTextAsEntryID)
+			{
+				textType = text.text;
+			}
+
+			ReloadText();
+			EventManager.AddListener<LanguageChangedEvent>(ReloadText);
+		}
+
+		private void ReloadText()
+		{
+			SetText(LocalisationUtil.GetLocalisedString(textType));
+		}
+
+		private void SetText(string newText)
+		{
+			if (text)
+			{
+				text.text = newText;
+			}
+
+			if (textTMP)
+			{
+				textTMP.text = newText;
+			}
+		}
+
+		private void OnDestroy()
+		{
+			EventManager.RemoveListener<LanguageChangedEvent>(ReloadText);
+		}
+	}
+}
