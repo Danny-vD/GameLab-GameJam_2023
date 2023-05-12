b5242c02f207e31ea820779cfc7c4ea27a082691
Danny-vD <dvduist@hotmail.nl>
2023-05-12 11:38:23 +0200

    Add script that enables the text depending on which interactable is entered

---
+++ GameLab-GameJam_2023/Assets/1. Scripts/UI/Interaction/InteractionUI.cs
@@ -0,0 +1,62 @@
+﻿using Events.Gameplay.Interaction;
+using Gameplay.Interactables;
+using Interfaces;
+using UnityEngine;
+using VDFramework;
+
+namespace UI.Interaction
+{
+	public class InteractionUI : BetterMonoBehaviour
+	{
+		[SerializeField]
+		private GameObject hostageObject;
+
+		[SerializeField]
+		private GameObject jewelryObject;
+
+		[SerializeField]
+		private GameObject moneyObject;
+
+		private void Start()
+		{
+			CanInteractEvent.Listeners                 += ShowObject;
+			CannotInteractEvent.ParameterlessListeners += HideAll;
+		}
+
+		private void ShowObject(CanInteractEvent canInteractEvent)
+		{
+			Show(canInteractEvent.Interactable);
+		}
+
+		private void Show(IInteractable interactable)
+		{
+			HideAll();
+
+			switch (interactable)
+			{
+				case HostageInteract:
+					hostageObject.SetActive(true);
+					break;
+				case JewelryInteract:
+					jewelryObject.SetActive(true);
+					break;
+				case MoneyInteract:
+					moneyObject.SetActive(true);
+					break;
+			}
+		}
+
+		private void HideAll()
+		{
+			hostageObject.SetActive(false);
+			jewelryObject.SetActive(false);
+			moneyObject.SetActive(false);
+		}
+
+		private void OnDestroy()
+		{
+			CanInteractEvent.Listeners                 -= ShowObject;
+			CannotInteractEvent.ParameterlessListeners -= HideAll;
+		}
+	}
+}
